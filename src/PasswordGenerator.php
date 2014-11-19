6a52df156a86ebbfc9e222412ea5782b8da1975b
billouboq <aurelien.boquet.web@gmail.com>
2014-11-19 19:23:20 +0100

    Initial commit

---
+++ src/PasswordGenerator.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bill
+ * Date: 18/11/2014
+ * Time: 11:57
+ */
+
+namespace Web1\StringGenerator;
+
+/**
+ * Class PasswordGenerator
+ * @package Web1\StringGenerator
+ */
+class PasswordGenerator
+{
+    /**
+     *
+     */
+    const PASSWORD_EASY     = 0;
+    /**
+     *
+     */
+    const PASSWORD_MEDIUM   = 1;
+    /**
+     *
+     */
+    const PASSWORD_HARD     = 2;
+
+    /**
+     * @var string
+     */
+    private static $passwordCharEasy   = 'abcdefghijklmnopqrstuvwxyz';
+    /**
+     * @var string
+     */
+    private static $passwordCharMedium = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    /**
+     * @var string
+     */
+    private static $passwordCharHard   = '#=!$£*+-.éè@';
+    /**
+     * @var int
+     */
+    private static $passwordDefaultLenght = 10;
+
+
+    /**
+     * @param null $number
+     * @param int $strength
+     * @return string
+     * @throws \Exception
+     */
+    public static  function getRandomString($number = null, $strength = self::PASSWORD_MEDIUM)
+    {
+        if (!in_array($strength, [
+            self::PASSWORD_EASY,
+            self::PASSWORD_MEDIUM,
+            self::PASSWORD_HARD,
+        ]))
+            throw new \Exception('Bad strength!');
+
+        $length = (is_null($number))
+            ? self::$passwordDefaultLenght
+            : (0 === (int)$number)
+                ? self::$passwordDefaultLenght
+                : (int)$number;
+
+        $password = '';
+
+        switch ($strength) {
+            case self::PASSWORD_EASY:
+                $char = self::$passwordCharEasy;
+                break;
+            case self::PASSWORD_MEDIUM:
+                $char = self::$passwordCharEasy.self::$passwordCharMedium;
+                break;
+            case self::PASSWORD_HARD:
+                $char = self::$passwordCharEasy.self::$passwordCharMedium.self::$passwordCharHard;
+                break;
+        }
+        for ($i = 0; $i < $length; $i++) {
+            $password .= mb_substr($char, mt_rand(0, (mb_strlen($char)-1)), 1);
+        }
+
+        return $password;
+    }
+}
