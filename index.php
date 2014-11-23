4d5e47cc778e686bffd5264cf859a165677cd6ef
Elodie <elodie.gonay@gmail.com>
2014-11-23 20:10:17 +0100

    Update

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-echo \Web1\StringGenerator\PasswordGenerator::getRandomString(100,\Web1\StringGenerator\PasswordGenerator::PASSWORD_HARD);
+//echo \Web1\StringGenerator\PasswordGenerator::getRandomString(100,\Web1\StringGenerator\PasswordGenerator::PASSWORD_HARD);
 
