3662a5f03ba26cd56b9ee370cd5d6a3485729743
Elodie <elodie.gonay@gmail.com>
2014-11-25 15:03:02 +0100

    neww

--- src/DateTimeValidator.php
+++ src/DateTimeValidator.php
@@ -10,18 +10,55 @@ namespace Php\Validator;
 
 use DateTime;
 
+/**
+ * Class DateTimeValidator
+ * @package Php\Validator
+ */
 class DateTimeValidator extends DateTime
 {
+    /**
+     * @param \DateTime $date
+     * @return bool
+     */
     public static function isDateFormat($date) {
         $datetime = DateTime::createFromFormat('Y-m-d', $date);
         return $datetime && $datetime->format('Y-m-d') == $date;
     }
 
+    /**
+     * @param \DateTime $datetime
+     * @param $test
+     * @return bool
+     */
     public static function isRightDate($datetime, $test) {
         return $datetime === $test;
     }
 
+    /**
+     * @param \DateTime $datetime
+     * @param $test
+     * @return bool
+     */
     public static function isAbove($datetime, $test) {
         return $datetime > $test;
+    }
+
+    /**
+     * @param \DateTime $datetime
+     * @param $test
+     * @return bool
+     */
+    public static function isLess($datetime, $test) {
+        return $datetime < $test;
+    }
+
+    /**
+     * @param \DateTime $datetime
+     * @param $test
+     * @param $test2
+     * @return bool
+     */
+    public static function isBetween($datetime, $test, $test2) {
+        return $test < $datetime && $datetime < $test2;
     }
 }
